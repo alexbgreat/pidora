aa6a5afb3f92398e757fa08afc59353f92d01ab6
Alex O'Neill <aboneill42@students.tntech.edu>
2014-11-24 11:52:11 -0600

    Add support to bar_update to save the station name whenever pianobar has an event

--- bar-update.py
+++ bar-update.py
@@ -8,8 +8,8 @@ def process(command, new = False):
 		with open(os.devnull, "w") as fnull: result = subprocess.Popen(command, stdout = fnull, stderr = fnull)
 	else:
 		with open(os.devnull, "w") as fnull: result = subprocess.call(command, stdout = fnull, stderr = fnull)
-def buildJSON(title, artist, album, artURL, loved, explainURL, songDuration = 0, songPlayed = 0):
-	data = '{"title": ' + json.dumps(title) + ',"artist": ' + json.dumps(artist) + ',"album": ' + json.dumps(album) + ',"artURL": ' + json.dumps(artURL) + ',"loved": ' + str(bool(loved)).lower() + ',"explainURL": ' + json.dumps(explainURL) + ', "songDuration": ' + str(songDuration) + ', "songPlayed": ' + str(songPlayed) + '}'
+def buildJSON(title, artist, album, artURL, station_name, loved, explainURL, songDuration = 0, songPlayed = 0):
+	data = '{"title": ' + json.dumps(title) + ',"artist": ' + json.dumps(artist) + ',"album": ' + json.dumps(album) + ',"artURL": ' + json.dumps(artURL) + ',"station_name": ' + json.dumps(station_name) + ',"loved": ' + str(bool(loved)).lower() + ',"explainURL": ' + json.dumps(explainURL) + ', "songDuration": ' + str(songDuration) + ', "songPlayed": ' + str(songPlayed) + '}'
 	return json.loads(data)
 def sendRequest(url, method, songData):
 	requests.post(url, params=dict(json=json.dumps(dict(method=method, id=1, songData=songData))))
@@ -28,11 +28,12 @@ rating = int(fields["rating"])
 detailUrl = fields["detailUrl"].split('?dc')[0]
 songDuration = fields["songDuration"]
 songPlayed = fields["songPlayed"]
+curStation = fields["stationName"]
 
 if event == "songstart" or event == "songexplain":
-	sendRequest(url, "SetSongInfo", buildJSON(title, artist, album, coverArt, rating, detailUrl))
+	sendRequest(url, "SetSongInfo", buildJSON(title, artist, album, curStation, coverArt, rating, detailUrl))
 elif event == "songlove":
-	sendRequest(url, "SetSongInfo", buildJSON(title, artist, album, coverArt, rating, detailUrl))
+	sendRequest(url, "SetSongInfo", buildJSON(title, artist, album, curStation, coverArt, rating, detailUrl))
 elif event == "usergetstations" or event == "stationcreate" or event == "stationdelete" or event == "stationrename":				# Code thanks to @officerNordBerg on GitHub
 	stationCount = int(fields["stationCount"])
 	stations = ""
